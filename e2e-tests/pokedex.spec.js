66813ce37b66579f5ad9e65d4222fe16c1be5048
AlessandroCaula <alecaula96@gmail.com>
2025-10-03 18:19:27 +0200

    Enhance CI/CD setup with Playwright for end-to-end testing and update configurations

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,24 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('') // http://localhost:8080/
+  })
+
+  test('Front page can be opened', async ({ page }) => {
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('Pokemon card can be opened', async({ page }) => {
+    await page.getByText('charizard').click()
+    await expect(page.getByText('Solar Power')).toBeVisible()
+  })
+
+  test('Pokemon card contains buttons to move around', async ({ page }) => {
+    await page.goto('/pokemon/charmander')
+    await expect(page.getByText('Previous')).toBeVisible()
+    await expect(page.getByText('Home')).toBeVisible()
+    await expect(page.getByText('Next')).toBeVisible()
+  })
+})
